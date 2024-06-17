52a64bb7edfa6e4c1a3ca0f9dfcb3d33f5086e3a
aduBoahene <boahenepatrick2@gmail.com>
2024-06-17 18:24:11 +0000

    added Logistics Solutions

--- app/logistics-solutions/page.tsx
+++ app/logistics-solutions/page.tsx
@@ -2,41 +2,42 @@
 
 import React, { useState } from "react";
 
-export default function page() {
+export default function Page() {
   const [activeTab, setActiveTab] = useState("tab1");
 
-  const handleTabClick = (tab: any) => {
+  const handleTabClick = (tab:any) => {
     setActiveTab(tab);
   };
+
   return (
-    <div className="md: border border-2">
+    <div className="border border-2">
       <div
-        className="relative flex items-center h-64 md:h-[600px] bg-cover"
+        className="relative flex items-center h-64 md:h-[600px] bg-cover bg-center"
         style={{ backgroundImage: `url('logistics-packing.jpeg')` }}
       >
-        <div className="w-full p-4 md:w-[70%] md:mx-auto bg-black-200">
+        <div className="w-full p-4 bg-opacity-75 bg-black-100 md:w-[70%] md:mx-auto">
           <h3
-            className="md: text-3xl text-white font-extrabold"
+            className="text-3xl text-white font-extrabold"
             style={{ fontFamily: "Montserrat-Medium" }}
           >
             Rondure E-Commerce
           </h3>
           <h3
-            className="md: text-3xl text-white font-extrabold"
+            className="text-3xl text-white font-extrabold"
             style={{ fontFamily: "Montserrat-Medium" }}
           >
             Logistics
           </h3>
 
           <p
-            className="md: mt-8 text-white text-xl md:w-3/5 leading-relaxed"
+            className="mt-4 md:mt-8 text-white text-xl md:w-3/5 leading-relaxed"
             style={{ fontFamily: "Montserrat-Medium" }}
           >
             Optimise your end-to-end e-commerce logistics, with order
             fulfilment, last-mile and cross-border delivery.
           </p>
 
-          <button className="bg-white rounded-lg py-4 px-6 text-black md:mt-4">
+          <button className="bg-white rounded-lg py-2 px-4 md:py-4 md:px-6 text-black mt-4 md:mt-8">
             Contact Us
           </button>
         </div>
@@ -44,51 +45,25 @@ export default function page() {
 
       <div className="border-b border-gray-200 dark:border-gray-700 md:w-[70%] md:mx-auto md:mt-8">
         <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-          <li className="me-2" style={{ fontFamily: "Montserrat-Medium" }} onClick={() => handleTabClick("tab1")} >
-            <a
-              href="#"
-              className="inline-flex items-center justify-center p-4 border-b-2 border-transparent active rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-              aria-current="page"
+          {["Overview", "Services", "Settings", "Global Coverage", "Insights"].map((tab, index) => (
+            <li
+              key={index}
+              className="me-2"
+              style={{ fontFamily: "Montserrat-Medium" }}
+              onClick={() => handleTabClick(`tab${index + 1}`)}
             >
-              Overview
-            </a>
-          </li>
-          <li className="me-2" style={{ fontFamily: "Montserrat-Medium" }} onClick={() => handleTabClick("tab2")} >
-            <a
-              href="#"
-              className="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-            >
-              Services
-            </a>
-          </li>
-          <li className="me-2" style={{ fontFamily: "Montserrat-Medium" }} onClick={() => handleTabClick("tab3")} >
-            <a
-              href="#"
-              className="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-            >
-              Settings
-            </a>
-          </li>
-          <li className="me-2" style={{ fontFamily: "Montserrat-Medium" }} onClick={() => handleTabClick("tab4")}          >
-            <a
-              href="#"
-              className="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-            >
-              Global Coverage
-            </a>
-          </li>
-          <li
-            className="me-2"
-            style={{ fontFamily: "Montserrat-Medium" }}
-             onClick={() => handleTabClick("tab5")}
-          >
-            <a
-              href="#"
-              className="inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-            >
-              Insights
-            </a>
-          </li>
+              <a
+                href="#"
+                className={`inline-flex items-center justify-center p-4 border-b-2 ${
+                  activeTab === `tab${index + 1}`
+                    ? "border-gray-300 text-gray-600"
+                    : "border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                } rounded-t-lg group`}
+              >
+                {tab}
+              </a>
+            </li>
+          ))}
         </ul>
         <div className="p-4">
           {activeTab === "tab1" && <div id="tab1">Tab 1</div>}
